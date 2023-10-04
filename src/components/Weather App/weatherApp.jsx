dc817f57967072ed698c48e6877fc173d555e463
Jasleen Kaur <jasleen2357@gmail.com>
2023-10-04 15:21:47 -0700

    Adding the folder structure for the weather app

---
+++ src/components/Weather App/weatherApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './weatherApp.css'
+// import search_icon from '../../Assets/search_icon.svg'
+// import clear_icon from '../../Assets/location_icon.svg'
+// import cloud_icon from '../../Assets/cloud_icon.svg'
+// import rain_icon from '../../Assets/rain_icon.svg'
+// import snow_icon from '../../Assets/snow_icon.svg'
+// import wind_icon from '../../Assets/thunder_icon.svg'
+// import drizzle_icon from '../../Assets/drizzle_icon.svg'
+// import humidity_icon from '../../Assets/humidity_icon.svg'
+
+
+function WeatherApp() {
+  return (
+    <div>WeatherApp</div>
+  )
+}
+
+export default WeatherApp
