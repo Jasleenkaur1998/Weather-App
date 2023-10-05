1a2d7abe618b7392239d274b975da61eacd80ce9
Jasleen Kaur <jasleen2357@gmail.com>
2023-10-04 22:36:08 -0700

    Adding the styling to the elements

--- src/components/Weather App/weatherApp.jsx
+++ src/components/Weather App/weatherApp.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-import './weatherApp.css'
-// import search_icon from '../../Assets/search_icon.svg'
-// import clear_icon from '../../Assets/location_icon.svg'
-// import cloud_icon from '../../Assets/cloud_icon.svg'
-// import rain_icon from '../../Assets/rain_icon.svg'
-// import snow_icon from '../../Assets/snow_icon.svg'
-// import wind_icon from '../../Assets/thunder_icon.svg'
-// import drizzle_icon from '../../Assets/drizzle_icon.svg'
-// import humidity_icon from '../../Assets/humidity_icon.svg'
-
+import React from "react";
+import "./weatherApp.css";
+import search_icon from "../Assets/search.png";
+import cloud_icon from "../Assets/cloud.png";
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
 
 function WeatherApp() {
   return (
-    <div>WeatherApp</div>
+    <div className="container">
+      <div className="top-bar">
+        <input
+          type="text"
+          className="city-input"
+          placeholder="Search for a city"
+        />
+        <div className="search-icon">
+          <img src={search_icon} alt="search icon" />
+        </div>
+      </div>
+      <div>
+        <div className="weather-image">
+          <img src={cloud_icon} alt="weather icon" />
+        </div>
+      </div>
+      <div className="weather-temp">24</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
