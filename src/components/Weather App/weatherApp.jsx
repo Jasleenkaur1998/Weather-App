b6e8876ae645486463799582ed96c66d4fb63f32
Jasleen Kaur <jasleen2357@gmail.com>
2023-10-06 11:09:09 -0700

    Adding the live weather data from the countries

--- src/components/Weather App/weatherApp.jsx
+++ src/components/Weather App/weatherApp.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react"
 import "./weatherApp.css"
-import search_icon from "../Assets/search.png"
 import cloud_icon from "../Assets/cloud.png"
 import wind_icon from "../Assets/wind.png"
 import humidity_icon from "../Assets/humidity.png"
 
 function WeatherApp() {
   const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY
-  const [city, setCity] = useState(null)
+  const [search, setSearch] = useState("")
+  const [weatherData, setWeatherData] = useState(null)
 
-  useEffect(() => {
+  const searchPressed = () => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data, "Connected to API")
-        setCity(data.data)
+        setWeatherData(data)
       })
-      .catch((error) => console.log(error))
-  }, [])
+  }
+
+  useEffect(() => {
+    if (weatherData) {
+      console.log(weatherData)
+    }
+  }, [weatherData])
 
   return (
     <div className="container">
@@ -28,30 +32,41 @@ function WeatherApp() {
           type="text"
           className="city-input"
           placeholder="Search for a city"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <div className="search-icon">
-          <img src={search_icon} alt="search icon" />
-        </div>
+        <button className="search-icon" onClick={searchPressed}>
+          <i className="fa fa-search" aria-hidden="true" />
+        </button>
       </div>
       <div>
         <div className="weather-image">
           <img src={cloud_icon} alt="weather icon" />
         </div>
       </div>
-      <div className="weather-temp">24</div>
-      <div className="weather-location">London</div>
+      <div className="weather-temp">
+        {" "}
+        {weatherData ? `${weatherData.main.temp}°C` : "No data"}
+      </div>
+      <div className="weather-location">
+        {weatherData ? `${weatherData.name}` : "No country selected"}
+      </div>
       <div className="data-container">
         <div className="element">
           <img src={humidity_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percentage">64%</div>
+            <div className="humidity-percentage">
+              {weatherData ? `${weatherData.main.humidity}` : "No data"}
+            </div>
             <div className="text">Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percentage">18 km/h</div>
+            <div className="humidity-percentage">
+              {weatherData ? `${weatherData.wind.speed}` : "No data"}
+            </div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
